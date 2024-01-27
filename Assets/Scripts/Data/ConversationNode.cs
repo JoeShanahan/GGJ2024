0af96000d18825f0dab24cd9d07a5e251589d02c
JoeLaptop <iam3Djoe@gmail.com>
2024-01-27 09:47:55 +0000

    Created conversation node objects

---
+++ Assets/Scripts/Data/ConversationNode.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ConversationNode : ScriptableObject
+{
+    public List<ConversationLine> DialogueLines;
+    public List<ConverstaionChoice> Choices;
+}
+
+[System.Serializable]
+public class ConversationLine
+{
+    public string LineText;
+    public string PersonName;
+    public Sprite PersonIcon;
+    public AudioClip TalkSound;
+}
+
+[System.Serializable]
+public class ConverstaionChoice
+{
+    public string ChoiceText;
+    public ItemData RequiredItem;
+    public ConversationNode NextNode;
+}
